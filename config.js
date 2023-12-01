f73b3ecaf593a0befa4cb83040870d8e6eb77fee
Camille Loue <camille.loue28@gmail.com>
2023-12-01 12:12:01 +0000

    update domainName in config

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const config = {
   appDescription:
     "All your bank accounts from around the world in one dashboard.",
   // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
-  domainName: "shipfa.st",
+  domainName: "netglobal.io",
   crisp: {
     // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (mailgun.supportEmail) otherwise customer support won't work.
     id: "",
